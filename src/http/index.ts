d4582d13e7ecc8c5a40142bf94af2ffba2aeaa3b
穆瑞新 <3274333659@qq.com>
2023-01-19 21:04:17 +0800

    third 2023-1-19

--- src/http/index.ts
+++ src/http/index.ts
@@ -12,7 +12,31 @@ const expModel = {
     },
     //删除实验，未连接好
     delExp:(id:any) =>{
-        return axios.delete(http.value + "/api/v1/exp/expDel", id)
+        return axios.delete(http.value + "/api/v1/exp/expDel" + id)
+    },
+    //提交修改实验信息
+    updateExp:(param:any)=>{
+        return axios.post(http.value + "/api/v1/exp/updateExp", param)
+    },
+    //模糊查询实验信息
+    selectExp:(name:any)=>{
+        return axios.get(http.value + "/api/v1/exp/selectExp", name)
+    },
+    //添加实验信息
+    addExp:(param:any)=>{
+        return axios.post(http.value + "/api/v1/exp/addOneExp", param)
+    },
+    //查看上传的图片
+    viewPhoto:(id:any)=>{
+        return axios.get(http.value + "/api/v1/exp/getExpImage" +id)
+    },
+    //提交图片
+    commitPhoto:(id:any, param:any)=>{
+        return axios.post(http.value + "/api/v1/exp/postExpImage" + id, param)
+    },
+    //删除图片
+    delPhoto:(req:any)=>{
+        return axios.post(http.value + "/api/v1/exp/deleteExpImage" + req)
     }
     
 }
